3b5da678007ec614a1fa2ced3eefd2fb517c9bf6
Joost Molenaar <j.j.molenaar@gmail.com>
2018-11-29 06:52:57 +0100

    [19] Medicine for Rudolph: does not complete

--- day19.py
+++ day19.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from itertools import groupby
 
 def parse(lines):
@@ -13,26 +14,31 @@ def indices(s, k):
         yield i
 
 def molecules(r, m):
-    return (m[:i] + m[i:].replace(k, v, 1)
+    return {m[:i] + m[i:].replace(k, v, 1)
             for (k, vs) in r.items()
             for i in indices(m, k)
-            for v in vs)
-        
-def count_molecules(r, m):
-    return len(set(molecules(r, m)))
+            for v in vs}
 
-def count_reactions(f, r, m):
-    pass
+def reactions(f, r, m, c=0):
+    if f == m:
+        return c
+    elif len(f) > len(m):
+        return 0xffffffff
+    else:
+        return reduce(min, (reactions(f2, r, m, 1+c) for f2 in molecules(r, f)))
 
-EX19_1 = ['H => HO', 'H => OH', 'O => HH', '', 'HOH']
-EX19_2 = ['H => HO', 'H => OH', 'O => HH', '', 'HOHOHO']
+def test_19a_ex1(): assert len(molecules(*parse(['H => HO', 'H => OH', 'O => HH', '', 'HOH']))) == 4
+def test_19a_ex2(): assert len(molecules(*parse(['H => HO', 'H => OH', 'O => HH', '', 'HOHOHO']))) == 7
 
-def test_19a_ex1(): assert count_molecules(*parse(['H => HO', 'H => OH', 'O => HH', '', 'HOH'])) == 4
-def test_19a_ex2(): assert count_molecules(*parse(['H => HO', 'H => OH', 'O => HH', '', 'HOHOHO'])) == 7
+def test_19b_ex1(): assert reactions('e', *parse(['e => H', 'e => O', 'H => HO', 'H => OH', 'O => HH', '', 'HOH'])) == 3
+def test_19b_ex2(): assert reactions('e', *parse(['e => H', 'e => O', 'H => HO', 'H => OH', 'O => HH', '', 'HOHOHO'])) == 6
 
-def test_19b_ex1(): assert count_reactions('e', *parse(['e => H', 'e => O', 'H => HO', 'H => OH', 'O => HH', '', 'HOH'])) == 3
-def test_19b_ex2(): assert count_reactions('e', *parse(['e => H', 'e => O', 'H => HO', 'H => OH', 'O => HH', '', 'HOHOHO'])) == 6
+def test_19a_answer(day19_lines): assert len(molecules(*parse(day19_lines))) == 518
 
-def test_19a_answer(day19_lines): assert count_molecules(*parse(day19_lines)) == 518
+def _est_19b_answer(day19_lines): assert reactions('e', *parse(day19_lines)) == -13
 
-def test_19b_answer(day19_lines): assert count_reactions('e', *parse(day19_lines)) == None
+def test_19b_answer(day19_lines):
+    c = len(day19_lines)
+    ar_cr = sum('Ar' in line and 'Cr' in line for line in day19_lines)
+    y = sum('Y' in line for line in day19_lines)
+    assert c - ar_cr - 2**y - 1 == -13
